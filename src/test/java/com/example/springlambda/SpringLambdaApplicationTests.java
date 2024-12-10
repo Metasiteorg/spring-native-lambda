744e98feef2c1a874deb0951d80a81351dfd49ce
Danielius Jurna <danielius.jurna@metasite.net>
2024-12-10 12:12:26 +0200

    Build refactor

--- src/test/java/com/example/springlambda/SpringLambdaApplicationTests.java
+++ src/test/java/com/example/springlambda/SpringLambdaApplicationTests.java
@@ -2,9 +2,18 @@ package com.example.springlambda;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class SpringLambdaApplicationTests {
+
+    @Container
+    @ServiceConnection
+    private static final MongoDBContainer MONGO_DB_CONTAINER = new MongoDBContainer();
 
     @Test
     void contextLoads() {
